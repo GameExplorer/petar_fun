2e7ce0ff24f85e76eca757e8768fd0035fef2ef6
GameExplorer <petar.petrovic2001@gmail.com>
2023-11-08 18:00:34 +0100

    New Zoom in Effect

--- src/components/MountainsSite.vue
+++ src/components/MountainsSite.vue
@@ -15,6 +15,20 @@ export default {
         <div class="bg-[#053B50] py-32">
             GORE +
             8K MOUNTAINS
+            Everest 8848m / 29028ft
+K2 8611m / 28250ft
+Kanchenjunga 8586m / 28169ft
+Lhotse 8516m / 27940ft
+Makalu 8463m / 27766ft
+Cho Oyu 8201m / 26906ft
+Dhaulagiri 8167m / 26795ft
+Manaslu 8163m / 26781ft
+Nanga Parbat 8125m / 26660ft
+Annapurna I 8091m / 26545ft
+Gasherbrum I 8068m / 26469ft
+Broad Peak 8047m / 26400ft
+Gasherbrum II 8035m / 26362ft
+Shishapangma 8012m / 26285ft
         </div>
         <div class="bg-[#176B87] py-32">
                 gore 5 - 7.9K MOUNTAINS
@@ -23,15 +37,29 @@ export default {
                 GORE 2 - 4.9K MOUNTAINS
         </div>
         <div class="bg-[#eeeeee] mountains2k grid grid-cols-8 gap-16">
-            <div>01</div>
-            <div>02</div>
-            <div>03</div>
-            <div>04</div>
-            <div class="col-span-2 fadein">
-                <img src="@/assets/Images/test.webp" alt="Everest"/>
+            <div class="col-span-2">01</div>
+            <div class="col-span-2">
+                <img src="public/Images/test.webp" alt="Everest"/>
             </div>
-            <div>06</div>
-            <div>07</div>
+            <div class="col-span-2">03</div>
+            <div class="col-span-2">04</div>
+            <!-- classic zoom-->
+            <div class="col-span-2">
+                <div class="growImage">
+                    <img src="public/Images/test2.png" alt="Everest"/>
+                    <span>Everest 8848m / 29028ft</span>
+                </div>
+            </div>
+            <!-- alternative zoom-->
+            <div class="col-span-2">
+                <div class="group/item growImage">
+                    <img src="public/Images/test2.png" alt="Everest"/>
+                    <div class="group-hover/edit invisible group-hover/item:visible">
+                        <span class="group-hover/opacity-100">Everest 8848m / 29028ft</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-span-2">07</div>
             <div>08</div>
             <div>09</div>
             <div>10</div>
@@ -41,7 +69,6 @@ export default {
             <div>14</div>
             <div>15</div>
             <div>16</div>
-
                 GORE 0 - 1.9K MOUNTAINS/HILLS
         </div>
     </div>
@@ -60,15 +87,23 @@ body {
 .mountains2k {
     padding-top: 10rem;
     padding-bottom: 50rem;
+    padding-left: 5rem;
 }
 
-.fadein img {
+
+.growImage {
     opacity: 0.5;
     transition: 1s ease;
 }
 
-.fadein img:hover {
-    opacity: 1;
+.growImage:hover {
+    /*opacity: 1;*/
+    transform: scale(1.1);
     transition: 1s ease;
+    /*box-shadow: 15px 10px 15px rgba(16, 15, 8, 0.944);*/
+    filter: drop-shadow(15px 0 12px rgba(0, 0, 0, 0.7));
+    transform: translateX(-4px);
+    opacity: 1;
 }
+
 </style>
