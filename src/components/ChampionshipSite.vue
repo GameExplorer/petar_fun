e2e5a1eb1053b031c6ec53eb95e03b7709b91ddb
GameExplorer <petar.petrovic2001@gmail.com>
2023-12-03 18:25:51 +0100

    New positions of the layout
    - Added columns
    - Added stats for RB
    - Added style for buttons
    - Started working on lists of stats

--- src/components/ChampionshipSite.vue
+++ src/components/ChampionshipSite.vue
@@ -1,54 +1,124 @@
 <template>
-  <div class="bg-[#00192D] text-white outline-8" :class="currentTeamClass">
-    <div class="text-4xl text-center pt-24">
-      How many days has passed since teams' last F1 Championship
-    </div>
-    <div class="text-center mt-24 text-4xl">
-      <div v-if="buttonClickedRB">
-        Days since RedBull last championship: <br />
-        <div class="text-8xl text-yellow-400 font-bold pt-4 pb-2">
-          {{ redBullDate }}
+  <div
+    class="bg-[#00192D] text-white outline-8 grid grid-cols-3 gap-8"
+    :class="currentTeamClass"
+  >
+    <div class="col-span-1">
+      <div
+        id="stats"
+        class="left mt-64 mx-8 px-6 pl-4 pt-2 pb-4 text-xl border-2 border-pink-400 float-left position-absolute shadow-[0_35px_100px_-15px_rgba(0,0,0,0.3)] shadow-purple-400"
+      >
+        <div v-if="buttonClickedRB">
+          <div class="mt-2 mb-2 font-bold text-center">List of Stats</div>
+          <div>Race debut: <span class="font-semibold">2005</span></div>
+          <div>Race starts: <span>369</span></div>
+          <div>Poles: <span>95</span></div>
+          <div>Podiums: <span>264</span></div>
+          <div>Wins: <span>113</span></div>
+          <div>Fastest laps: <span>95</span></div>
+          <div>Points: <span>7248</span></div>
+          <div>Constructors Championship: <span>6</span></div>
+          <div>Drivers Championship: <span>7</span></div>
         </div>
-        <div class="pt-2 pb-4">Date: 10/24/2023 - Suzuka</div>
-      </div>
-      <div v-else-if="buttonClickedFr">{{ ferrariDate }}</div>
-      <div v-else-if="buttonClickedAt">Has Never Won Constructor</div>
-    </div>
-    <div id="stats" class="left pl-8 border-2 float-left">
-      <div class="mt-12">List of Stats</div>
-      <div>Wins:</div>
-      <div>Podiums:</div>
-      <div>Poles:</div>
-      <div>Race starts:</div>
-      <div>Championships:</div>
-      <div>Since:</div>
-    </div>
-    <div id="champions" class="right pr-8 border-2 float-right">
-      <h5>List of Constructors Years</h5>
-      <div v-if="buttonClickedRB">
-        <div>2010</div>
-        <div>2011</div>
-        <div>2012</div>
-        <div>2013</div>
       </div>
     </div>
-    <div class="grid object-none object-bottom place-items-center pb-12">
+    <div class="col-span-1">
+      <div class="text-4xl text-center pt-24">
+        How many days has passed since teams' last F1 Championship
+        <div class="text-center mt-24 text-4xl">
+          <div v-if="buttonClickedRB">
+            Days since RedBull last championship: <br />
+            <div class="text-8xl text-yellow-400 font-bold pt-4 pb-2">
+              {{ redBullDate }}
+            </div>
+            <div class="pt-2 pb-8">Date: 10/24/2023 - Suzuka</div>
+          </div>
+          <div class="pt-2 pb-8" v-else-if="buttonClickedFr">
+            Days since Ferrari last championship: <br />
+            <div class="text-7x text-white font-bold pt-4 pb-2">
+              {{ ferrariDate }}
+            </div>
+          </div>
+          <div class="pt-2 pb-8" v-else-if="buttonClickedAt">
+            Has Never Won Constructor
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-span-1">
+      <div
+        id="champions"
+        class="right mt-64 mr-8 pr-8 text-xl border-2 float-right position-absolute"
+      >
+        <h5>List of Constructors Years</h5>
+        <div>
+          <span
+            >2010 2011 2012 2013<br />
+            2022 2023</span
+          >
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="grid object-none object-bottom place-items-center pb-12 pt-20 col-span-4"
+    >
       <ul class="inline mx-2">
         <button
-          class="mx-4 bg-white text-black px-4 rounded-lg text-2xl"
+          class="mx-4 px-4 bg-white text-black text-2xl rounded-lg"
           @click="toggleRedBull"
         >
           RedBull
         </button>
-        <button class="mx-2" @click="toggleFerrari">Ferrari</button>
-        <button class="mx-2" @click="toggleMercedes">Mercedes</button>
-        <button class="mx-2" @click="toggleMclaren">Mclaren</button>
-        <button class="mx-2">Aston Martin</button>
-        <button class="mx-2">Alpine</button>
-        <button class="mx-2" @click="toggleAlphaTauri">Alpha Tauri</button>
-        <button class="mx-2">Williams</button>
-        <button class="mx-2">Alfa Romeo</button>
-        <button class="mx-2">Haas</button>
+        <button
+          class="mx-4 px-4 bg-white text-black text-2xl rounded-lg border-2"
+          @click="toggleFerrari"
+        >
+          Ferrari
+        </button>
+        <button
+          class="mx-4 px-4 bg-white text-black text-2xl rounded-lg border-2"
+          @click="toggleMercedes"
+        >
+          Mercedes
+        </button>
+        <button
+          class="mx-4 px-4 bg-white text-black text-2xl rounded-lg border-2"
+          @click="toggleMclaren"
+        >
+          Mclaren
+        </button>
+        <button
+          class="mx-4 px-4 bg-white text-black text-2xl rounded-lg border-2"
+        >
+          Aston Martin
+        </button>
+        <button
+          class="mx-4 px-4 bg-white text-black text-2xl rounded-lg border-2"
+        >
+          Alpine
+        </button>
+        <button
+          class="mx-4 px-4 bg-white text-black text-2xl rounded-lg border-2"
+          @click="toggleAlphaTauri"
+        >
+          Alpha Tauri
+        </button>
+        <button
+          class="mx-4 px-4 bg-white text-black text-xl rounded-lg border-2"
+        >
+          Williams
+        </button>
+        <button
+          class="mx-4 px-4 bg-white text-black text-2xl rounded-lg border-2"
+        >
+          Alfa Romeo
+        </button>
+        <button
+          class="mx-4 px-4 bg-white text-black text-2xl rounded-lg border-2"
+        >
+          Haas
+        </button>
       </ul>
     </div>
   </div>
@@ -76,7 +146,7 @@ export default {
       const today = new Date();
       const diffTime = Math.abs(today - days);
       const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24) - 1);
-      return `Days since last championship: ${diffDays}`;
+      return `${diffDays}`;
     },
     alphaTauriDate() {
       return `Has never won constructor`;
