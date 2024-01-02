7dcb2c8a3c7cfa5d966a368d718158eeb6952f30
GameExplorer <petar.petrovic2001@gmail.com>
2024-01-02 17:38:55 +0100

    Updated HomePage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,28 @@
-<script setup>
-</script>
 
+<style>
+ul {
+    display: flex;
+    justify-content: space-around;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+</style>
 <template>
+  <!-- add Images to the router links and make it like a grid 3x3 or 2x2-->
     <ul>
-        <li><RouterLink to="/">Home</RouterLink></li>
-        <li><RouterLink to="/thrones">Throne</RouterLink></li>
-        <li><RouterLink to="/mountains">Mountains</RouterLink></li>
+        <li><RouterLink :to="{ name: 'Thrones'}">Throne</RouterLink></li>
+        <li><RouterLink :to="{ name: 'Mountains'}">Mountains</RouterLink></li>
+        <li><RouterLink :to="{ name: 'Champions'}">Champions</RouterLink></li>
   </ul>
 </template>
 
-<style scoped>
 
-</style>
+<script>
+import { RouterLink } from 'vue-router';
+
+export default {
+    name: 'HomeView',
+    components: { RouterLink }
+}
+</script>
