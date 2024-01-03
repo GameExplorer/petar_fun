35ada3eec01dc427726b07f10687a0bc3b137709
GameExplorer <petar.petrovic2001@gmail.com>
2024-01-03 21:17:37 +0100

    1961 - Ferrari

--- src/components/ChampionshipSite.vue
+++ src/components/ChampionshipSite.vue
@@ -1207,19 +1207,16 @@
       </ul>
     </div>
   </div>
-  <!-- chequred flag border WORKING-->
-  <div class="silver">
-    <div class="border-b-[16px] border-black border-dashed"></div>
-    <div class="border-b-[16px] mx-6 border-black border-dashed"></div>
-    <div class="border-b-[16px] border-black border-dashed"></div>
-    <div class="border-b-[16px] mx-6 border-black border-dashed"></div>
-  </div>
-  <!-- Winning Years for each team -- WORKING -->
+  <!-- chequred flag border TO DO-->
   <div>
-    <div v-if="buttonClickedRB" class="redbull text-white grid ">
+  </div>
+  <!-- Winning Years for each team -- TO DO -->
+  <div>
+    <div v-if="buttonClickedRB" class="redbull text-white grid">
       <div id="2010" class="p-12 mb-6">
         <div class="text-4xl md:text-5xl text-left font-bold py-10">
-          <span class="pr-8 border-red-400 border-b-2 transition-all duration-500 hover:pr-28 hover:border-b-4 hover:text-6xl"
+          <span
+            class="pr-8 border-red-400 border-b-2 transition-all duration-500 hover:pr-28 hover:border-b-4 hover:text-6xl"
             >MAIDEN TITLES WINS IN 2010</span
           >
         </div>
@@ -1227,8 +1224,7 @@
           <img
             src="/Images/vettel2010.png"
             alt="VettelWinsFirstTitle"
-            class="xl:h-2/5 xl:w-2/4 lg:h-2/5 lg:w-2/4 md:h-2/5 md:w-2/4 sm:h-1/4 sm:w-1/4 ml-2 mr-12 float-left cursor-default
-            rbimg "
+            class="xl:h-2/5 xl:w-2/4 lg:h-2/5 lg:w-2/4 md:h-2/5 md:w-2/4 sm:h-1/4 sm:w-1/4 ml-2 mr-12 float-left cursor-default rbimg"
           />
           <div class="pt-8 pb-4">
             2010 season was one of the more memorable seasons in F1. New points
@@ -1245,14 +1241,15 @@
             secure the title. The season saw Vettel, Alonso, Webber and Hamilton
             battling until the very end for the Driver's title. Vettel's victory
             came after a dramatic season finale at Abu Dhabi. He became the
-            youngest formula 1 champion at the age of 23 years and 134 days.<br>
-            <p class="text-right text-gray-500 italic">Source: es.motorsport </p>
+            youngest formula 1 champion at the age of 23 years and 134 days.<br />
+            <p class="text-right text-gray-500 italic">Source: es.motorsport</p>
           </div>
         </div>
       </div>
       <div id="2011" class="p-12 mb-6">
         <div class="text-4xl md:text-5xl text-left font-bold pb-16">
-          <span class="pr-8 border-red-400 border-b-2 transition-all duration-500 hover:pr-28 hover:border-b-4 hover:text-6xl"
+          <span
+            class="pr-8 border-red-400 border-b-2 transition-all duration-500 hover:pr-28 hover:border-b-4 hover:text-6xl"
             >BACK TO BACK TITLE WINS</span
           >
         </div>
@@ -1267,15 +1264,20 @@
             the inaugural running of the Indian Grand Prix. Vettel and Red Bull
             defended their titles in 2011. Vettel won the title with 392 points,
             122 points ahead of Jenson Button. While Red Bull won the
-            constructor's title with 650 points, 153 points ahead of McLaren.<br>
-            <p class="text-right text-gray-500 italic">Image source: The Telegraph</p>
+            constructor's title with 650 points, 153 points ahead of McLaren.<br />
+            <p class="text-right text-gray-500 italic">
+              Image source: The Telegraph
+            </p>
           </div>
         </div>
       </div>
       <div id="2012" class="p-12 mb-6">
         <div class="text-4xl md:text-5xl text-left font-bold py-10">
-          <span class="pr-8 border-red-400 border-b-2 transition-all duration-500 hover:pr-28 hover:border-b-4 hover:text-6xl">
-            HAT TRICK IN 2012</span>
+          <span
+            class="pr-8 border-red-400 border-b-2 transition-all duration-500 hover:pr-28 hover:border-b-4 hover:text-6xl"
+          >
+            HAT TRICK IN 2012</span
+          >
         </div>
         <div class="text-xl">
           <img
@@ -1300,21 +1302,26 @@
             with 460 points, 60 points ahead of Ferrari in the United States.<br />
             The season had the most races ever in a season with twenty and also
             six World Driver's Champions on the grid. This was Schumacher's last
-            season in Formula.<br>
-            <p class="text-right text-gray-500 italic">Image source: Drive.com.au</p>
+            season in Formula.<br />
+            <p class="text-right text-gray-500 italic">
+              Image source: Drive.com.au
+            </p>
           </div>
         </div>
       </div>
       <div id="2013" class="p-12 mb-6">
         <div class="text-5xl text-left font-bold py-10">
-          <span class="pr-8 border-red-400 border-b-2 transition-all duration-500 hover:pr-28 hover:border-b-4 hover:text-6xl">
-            FOUR IN A ROW</span>
+          <span
+            class="pr-8 border-red-400 border-b-2 transition-all duration-500 hover:pr-28 hover:border-b-4 hover:text-6xl"
+          >
+            FOUR IN A ROW</span
+          >
         </div>
         <div class="text-xl">
           <img
             src="/Images/vettel2013.png"
             alt="VettelWinsFirstTitle"
-            class="xl:h-2/5 xl:w-2/4 lg:h-2/5 lg:w-2/4 md:h-2/5 md:w-2/4 sm:h-1/4 sm:w-1/4 mr-2 ml-12 float-right rbimg "
+            class="xl:h-2/5 xl:w-2/4 lg:h-2/5 lg:w-2/4 md:h-2/5 md:w-2/4 sm:h-1/4 sm:w-1/4 mr-2 ml-12 float-right rbimg"
           />
           <div class="pt-8 pb-4">
             The 2013 season was the final year the series used 2.4 litre V8
@@ -1331,15 +1338,19 @@
             Bull won the constructor's title in India with 596 points, 236
             points ahead of Mercedes. It remains the last for a Renault powered
             driver to win a title. The 2013 marked the end of the Vettel and Red
-            Bull run, Vettel never won a championship after that.<br>
-            <p class="text-right text-gray-500 italic">Image source: Sportskeeda</p>
+            Bull run, Vettel never won a championship after that.<br />
+            <p class="text-right text-gray-500 italic">
+              Image source: Sportskeeda
+            </p>
           </div>
         </div>
       </div>
       <div id="2022" class="p-12 mb-6">
         <div class="text-4xl md:text-5xl text-left font-bold py-10">
-          <span class="pr-8 border-red-400 border-b-2 transition-all duration-500 hover:pr-28 hover:border-b-4 hover:text-6xl cursor-default"
-            >THE START OF DOMINATION</span>
+          <span
+            class="pr-8 border-red-400 border-b-2 transition-all duration-500 hover:pr-28 hover:border-b-4 hover:text-6xl cursor-default"
+            >THE START OF THE DOMINATION</span
+          >
         </div>
         <div class="text-xl">
           <img
@@ -1359,38 +1370,43 @@
             grand prix in a season surpassing the Michael Schumacher and the
             retiring Sebastian Vettel. Despite having poor strategy Leclerc
             finished 2nd 146 Points behind Max. RedBull won the constructor's
-            title in Mexico with 700 points, 300 points ahead of Ferrari.<br>
+            title in Mexico with 700 points, 300 points ahead of Ferrari.<br />
             <p class="text-right text-gray-500 italic">Image source: WWLP</p>
           </div>
         </div>
       </div>
       <div id="2023" class="p-12 mb-12">
         <div class="text-5xl text-left font-bold py-10">
-          <span class="pr-8 border-red-400 border-b-2 transition-all duration-500 hover:pr-28 hover:border-b-4 hover:text-6xl cursor-default">
-            SUPER MAX!</span>
+          <span
+            class="pr-8 border-red-400 border-b-2 transition-all duration-500 hover:pr-28 hover:border-b-4 hover:text-6xl cursor-default"
+          >
+            SUPER MAX!</span
+          >
         </div>
         <div class="text-xl">
           <img
             src="/Images/verstappen2023.png"
             alt="VettelWinsFirstTitle"
-            class="xl:h-2/5 xl:w-2/4 lg:h-2/5 lg:w-2/4 md:h-2/5 md:w-2/4 sm:h-1/4 sm:w-1/4 mr-2 ml-12 float-right  
-            rbimg"
+            class="xl:h-2/5 xl:w-2/4 lg:h-2/5 lg:w-2/4 md:h-2/5 md:w-2/4 sm:h-1/4 sm:w-1/4 mr-2 ml-12 float-right rbimg"
           />
           <div class="pt-8 pb-4">
-            The 2023 season was dominated by one single driver. MAX VERSTAPPEN. After Checo
-            beat Max in Baku, Max said to his father he will never lose to him again. He went on to
-            win 19 out of 22 Grand Prixs, failing to win in Singapore where he struggled the most and in
-            Saudia Arabia and Azerbaijan where Perez was better. Max won the title with whopping 500 points, 300 points
-            ahead of Perez. RedBull won the constructor's title in Mexico with 800 points, 400 points ahead of Ferrari.
-            Max had more points than any 2nd place Ferrari. He could have won Constructor's title all by himself.
-            RedBull won 21 out of 22 Grand Prix beating Mclaren record from 1988.
-
+            The 2023 season was dominated by one single driver. MAX VERSTAPPEN.
+            After Checo beat Max in Baku, Max said to his father he will never
+            lose to him again. He went on to win 19 out of 22 Grand Prixs,
+            failing to win in Singapore where he struggled the most and in
+            Saudia Arabia and Azerbaijan where Perez was better. Max won the
+            title with whopping 500 points, 300 points ahead of Perez. RedBull
+            won the constructor's title in Mexico with 800 points, 400 points
+            ahead of Ferrari. Max had more points than any 2nd place Ferrari. He
+            could have won Constructor's title all by himself. RedBull won 21
+            out of 22 Grand Prix beating Mclaren record from 1988.
           </div>
           <div>
-            Max broke numerous records including most wins, most podiums, most consecutive wins, most wins from pole,
-            most points, most hat tricks (pole, win, fastest lap), biggest championship winning margin, most laps led,
-            highest percentage of laps led, most sprint wins and only driver to win three times in one country during 
-            a single season<br>
+            Max broke numerous records including most wins, most podiums, most
+            consecutive wins, most wins from pole, most points, most hat tricks
+            (pole, win, fastest lap), biggest championship winning margin, most
+            laps led, highest percentage of laps led, most sprint wins and only
+            driver to win three times in one country during a single season<br />
             <p class="text-right text-gray-500 italic">Image source: F1.com</p>
           </div>
         </div>
@@ -1401,42 +1417,35 @@
     <div v-if="buttonClickedFr" class="ferrari text-white grid">
       <div id="1961" class="p-12 mb-6">
         <div class="text-4xl md:text-5xl text-left font-bold py-10">
-          <span class="pr-8 border-red-400 border-b-2 transition-all duration-500 hover:pr-28 hover:border-b-4 hover:text-6xl"
-            >MAIDEN TITLES WINS IN 2010</span
+          <span
+            class="pr-8 border-white border-b-2 transition-all duration-500 hover:pr-28 hover:border-b-4 hover:text-6xl"
+            >FIRST OF MANY</span
           >
         </div>
         <div class="text-xl">
           <img
-            src="/Images/vettel2010.png"
+            src="/Images/ferrari1961.png"
             alt="VettelWinsFirstTitle"
-            class="xl:h-2/5 xl:w-2/4 lg:h-2/5 lg:w-2/4 md:h-2/5 md:w-2/4 sm:h-1/4 sm:w-1/4 ml-2 mr-12 float-left cursor-default
-            rbimg "
+            class="xl:h-2/5 xl:w-2/4 lg:h-2/5 lg:w-2/4 md:h-2/5 md:w-2/4 sm:h-1/4 sm:w-1/4 ml-2 mr-12 float-left cursor-default"
           />
           <div class="pt-8 pb-4">
-            2010 season was one of the more memorable seasons in F1. New points
-            system was introduced and Mercedes re-entered Formula 1. This was
-            also Bridgestone's final season in the formula 1 as the company
-            decided to leave the sport after 14 years. Pirelli was then chosen
-            as the new tyre supplier for the sport. The season also was the
-            return of the seven time world champion Michael Schumacher. New
-            teams were also introduced to the sport, Hispania, Lotus and Virgin
-            Racing.
+            1961 Season was the first year of 1.5 litre formula and it was dominated by Ferrari. This year was the
+            first year they made mid-engine car going against Enzo's philosophy. The season was dominated by Phil
+            Hill and Wolfgang von Trips. Phil Hill won the title with 34 points, 1 point ahead of Wolfgang von Trips who
+            was killed at the Italian Grand Prix. Hill remains to this day only American to win Driver's title. 
+            Ferrari won the constructor's title with 57 points, 14 points ahead of  Lotus.<br />
           </div>
           <div>
-            Red Bull secured its maiden constructor in Brazil, finishing 1-2 to
-            secure the title. The season saw Vettel, Alonso, Webber and Hamilton
-            battling until the very end for the Driver's title. Vettel's victory
-            came after a dramatic season finale at Abu Dhabi. He became the
-            youngest formula 1 champion at the age of 23 years and 134 days.<br>
-            <p class="text-right text-gray-500 italic">Source: es.motorsport </p>
+            Did you know 1961 Dutch Grand Prix was a strange. Every starter finished the race and no-one went into the pits. 
+            Such reliability has never been achieved since
+            <br />
+            <p class="text-right text-gray-800 italic">Source: wikipedia</p>
           </div>
         </div>
       </div>
     </div>
   </div>
-  <div>
-    Footer
-  </div>
+  <div>Footer</div>
 </template>
 
 <script>
@@ -1709,18 +1718,14 @@ export default {
 </script>
 
 <style>
-
 .rbimg {
   --_g: 10% /45% 45% no-repeat linear-gradient(#000 0 0);
-  --m:
-    left   var(--_i,0%) top    var(--_g),
-    bottom var(--_i,0%) left   var(--_g),
-    top    var(--_i,0%) right  var(--_g),
-    right  var(--_i,0%) bottom var(--_g);
+  --m: left var(--_i, 0%) top var(--_g), bottom var(--_i, 0%) left var(--_g),
+    top var(--_i, 0%) right var(--_g), right var(--_i, 0%) bottom var(--_g);
   -webkit-mask: var(--m);
-          mask: var(--m);
+  mask: var(--m);
   filter: grayscale();
-  transition: .4s linear;
+  transition: 0.4s linear;
   cursor: pointer;
 }
 .rbimg:hover {
