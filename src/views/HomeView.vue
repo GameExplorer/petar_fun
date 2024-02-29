9eb40902b35a50bf35ec6729a5512810227b01ea
GameExplorer <petar.petrovic2001@gmail.com>
2024-02-29 19:15:06 +0100

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,9 @@
   <!-- add Images to the router links and make it like a grid 3x3 or 2x2-->
   <ul class="grid grid-rows grid-cols-2 gap-1 pt-12 px-32 m-12">
     <li class="flex items-center justify-center rounded-lg">
-      <RouterLink :to="{ name: 'Thrones' }">Thrones </RouterLink>
+      <RouterLink :to="{ name: 'Thrones' }">
+        <img src="/Images/F1Champions.png" alt="F1 Champions" />
+        </RouterLink>
     </li>
     <li class="flex items-center justify-center p-4 border-2">
       <RouterLink :to="{ name: 'Mountains' }">Mountains</RouterLink>
