ed28f4e7751a4161f80a2431f9493b567f1c3a4e
GameExplorer <petar.petrovic2001@gmail.com>
2024-02-29 18:48:26 +0100

    Removed few things

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,25 @@
-
-<style>
-ul {
-    display: flex;
-    justify-content: space-around;
-    list-style: none;
-    padding: 0;
-    margin: 0;
-}
-</style>
+<style></style>
 <template>
   <!-- add Images to the router links and make it like a grid 3x3 or 2x2-->
-  <ul class="grid grid-rows grid-cols-2 gap-4 pt-12">
-        <li class=" flex items-center justify-center p-4 border-2"><RouterLink :to="{ name: 'Thrones'}">Throne</RouterLink></li>
-        <li class=" flex items-center justify-center p-4 border-2"><RouterLink :to="{ name: 'Mountains'}">Mountains</RouterLink></li>
-        <li class=" flex items-center justify-center p-4 border-2"><RouterLink :to="{ name: 'Champions'}">Champions</RouterLink></li>
-        <li class=" flex items-center justify-center p-4 border-2">Solar System</li>
+  <ul class="grid grid-rows grid-cols-2 gap-1 pt-12 px-32 m-12">
+    <li class="flex items-center justify-center rounded-lg">
+      <RouterLink :to="{ name: 'Thrones' }">Thrones </RouterLink>
+    </li>
+    <li class="flex items-center justify-center p-4 border-2">
+      <RouterLink :to="{ name: 'Mountains' }">Mountains</RouterLink>
+    </li>
+    <li class="flex items-center justify-center p-4 border-2">
+      <RouterLink :to="{ name: 'Champions' }">Champions</RouterLink>
+    </li>
+    <li class="flex items-center justify-center p-4 border-2">Solar System</li>
   </ul>
 </template>
 
-
 <script>
-import { RouterLink } from 'vue-router';
+import { RouterLink } from "vue-router";
 
 export default {
-    name: 'HomeView',
-    components: { RouterLink }
-}
+  name: "HomeView",
+  components: { RouterLink },
+};
 </script>
