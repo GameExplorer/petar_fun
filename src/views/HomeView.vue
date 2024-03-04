d556d15d7ceb9920f6073242d978d46227e837f2
GameExplorer <petar.petrovic2001@gmail.com>
2024-03-04 20:53:50 +0100

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,40 @@
-<style></style>
+<style>
+@font-face {
+  font-family: "Archia";
+  src: url("@/assets/Fonts/archia-regular.woff") format("truetype");
+}
+
+.fontsMain {
+  font-family: "Rubik";
+}
+</style>
 <template>
-  <!-- add Images to the router links and make it like a grid 3x3 or 2x2-->
-  <ul class="grid grid-rows grid-cols-2 gap-1 pt-12 px-32 m-12">
-    <li class="flex items-center justify-center rounded-lg">
-      <RouterLink :to="{ name: 'Thrones' }">
-        <img src="/Images/F1Champions.png" alt="F1 Champions" />
+  <div class="fontsMain">
+    <div class="flex items-center justify-center pt-16">
+      <h1 class="text-4xl font-bold uppercase">PETAR.FUN</h1>
+    </div>
+    <!-- add Images to the router links and make it like a grid 3x3 or 2x2-->
+    <ul class="grid grid-rows grid-cols-3 pt-12 gap-1 px-64 mx-12">
+      <li class="flex items-center justify-center rounded-lg border-2">
+        <RouterLink :to="{ name: 'Thrones' }"> Thrones </RouterLink>
+      </li>
+      <li class="flex items-center justify-center">
+        <RouterLink :to="{ name: 'Mountains' }">
+          <div class="flex items-center justify-center">
+            Mountains
+          </div>
         </RouterLink>
-    </li>
-    <li class="flex items-center justify-center p-4 border-2">
-      <RouterLink :to="{ name: 'Mountains' }">Mountains</RouterLink>
-    </li>
-    <li class="flex items-center justify-center p-4 border-2">
-      <RouterLink :to="{ name: 'Champions' }">Champions</RouterLink>
-    </li>
-    <li class="flex items-center justify-center p-4 border-2">Solar System</li>
-  </ul>
+      </li>
+      <li class="flex items-center justify-center">
+        <RouterLink :to="{ name: 'Champions' }">
+          <div class="flex items-center justify-center">F1 Champions</div>
+        </RouterLink>
+      </li>
+      <li class="flex items-center justify-center p-4 border-2">
+        Solar System
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
