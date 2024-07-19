ac078c7d13b293303b5a5b5bb7d1e4ddaaadb08d
GameExplorer <petar.petrovic2001@gmail.com>
2024-07-19 22:16:58 +0200

    Update 1.28
    - Revamped the Thrones page
    - Many things to be still added

---
+++ src/components/Thrones/ThronesTextData.vue
@@ -0,0 +1,49 @@
+<template></template>
+
+<script>
+export default {
+  name: "thronesTextData",
+  props: ["daysPassed"],
+  data() {
+    return {
+      textData: [
+        {
+          number: Math.floor(this.daysPassed / 200),
+          text: " Times you could have travelled from King's Landing to the Wall",
+          image: "/Images/Thrones/got.avif",
+        },
+        {
+          number: Math.floor(this.daysPassed / 110),
+          text: " Times you could have read the entire A Song of Ice and Fire series",
+          info: "Around four months to complete the series",
+          image: "/Images/Thrones/got.avif",
+        },
+        {
+          number: Math.floor(this.daysPassed / 35),
+          text: " Times you could have watched the entire got series (You watch 2 episodes per day)",
+          image: "/Images/Thrones/got.avif",
+        },
+        {
+          number: Math.floor(this.daysPassed / 12),
+          text: " Times you could have watched the LOTR trilogy (extended edition)",
+          image: "/Images/Thrones/got.avif",
+        },
+
+        {
+          number: Math.floor(this.daysPassed / 225),
+          text: " Times you could have beaten the Witcher trilogy",
+        },
+
+        {
+          number: Math.floor(this.daysPassed / 554),
+          text: " Times you could have travelled around the world",
+        },
+        {
+          number: Math.floor(this.daysPassed * 42.2),
+          text: " Times you could have run"
+        },
+      ],
+    };
+  },
+};
+</script>
--- src/components/ThronesSite.vue
+++ src/components/ThronesSite.vue
@@ -1,15 +1,91 @@
+<template v-cloak>
+  <div class="text-Font">
+    <div class="background">
+      <div class="container mx-auto text-thrones">
+        <div class="text text-2xl md:text-6xl sm:text-xl py-12 text-center">
+          <h1 class="py-2">
+            <span class="md:text-7xl text-3xl daysText font-bold">{{
+              daysPassed
+            }}</span>
+            DAYS SINCE LAST
+          </h1>
+          <h1 class="py-2">ASOIAF BOOK</h1>
+          <h2 class="py-2 text-2xl">in the mean time you could have...</h2>
+        </div>
+      </div>
+    </div>
+    <div class="background2 py-2 items-center border-t-8 border-[#010101]">
+      <div class="m-auto p-0 text-center text-2xl">
+        <button
+          class="text-black bg-[#eaeaea] m-6 px-4 py-2 lg:px-12 lg:py-4 rounded-2xl uppercase border-4 border-gray-400 hover:scale-105 transform transition duration-500 ease-in-out"
+          @click="handleClick"
+        >
+          Winter is Coming
+        </button>
+      </div>
+    </div>
+    <div class="background2 mb-8 pb-32">
+      <div v-if="textData" class="container mx-auto mb-8 pb-32">
+        <div
+          :class="['box', currentClass]"
+          class="p-6 rounded-lg shadow-xl border-2 border-gray-600 bgImage"
+        >
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6 items-center">
+            <div class="md:col-span-1">
+              <img
+                v-if="textData.image"
+                :src="textData.image"
+                :alt="textData.text"
+                class="w-full h-auto rounded-xl shadow-md border-2 border-gray-600"
+              />
+            </div>
+            <div class="md:col-span-2 text-center">
+              <p class="text-5xl lg:text-6xl font-bold mb-4">
+                <span class="text-purple-400 animate-pulse">{{
+                  textData.number
+                }}</span>
+                <span class="text-gray-300"></span>
+                <span class="text-4xl lg:text-5xl font-semibold mb-4">
+                  {{ textData.text }} </span
+                ><br />
+                <span class="text-xl md:text-2xl italic text-gray-400">
+                  {{ textData.info }}
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <thronesTextData ref="thronesTextData" :daysPassed="daysPassed" />
+  </div>
+
+  <Footer></Footer>
+</template>
+
 <script>
 import "/src/style.css";
 import CountingDiv from "@/components/CountingDiv.vue";
+import Footer from "@/components/Footer/Footer.vue";
+import thronesTextData from "@/components/Thrones/ThronesTextData.vue";
 
 export default {
   name: "ThronesSite",
+  components: {
+    CountingDiv,
+    Footer,
+    thronesTextData,
+  },
   data() {
     return {
-      targetDate: new Date("2011-06-12"), // Replace with your target date
-      data: [{ books: "Martin has published 23" }],
+      targetDate: new Date("2011-06-12"),
+      textData: null,
+      colorClasses: ["targaryen", "stark", "lannister", "baratheon"],
+      currentClassIndex: 0,
     };
   },
+
   computed: {
     daysPassed() {
       const currentDate = new Date();
@@ -18,121 +94,55 @@ export default {
         Math.floor(timeDifference / (1000 * 60 * 60 * 24)) - 30; // Milliseconds to days
       return daysPassed;
     },
+    currentClass() {
+      return this.colorClasses[this.currentClassIndex];
+    },
   },
-  components: {
-    CountingDiv,
+
+  methods: {
+    generateText() {
+      if (this.$refs.thronesTextData && this.$refs.thronesTextData.textData) {
+        const textDataArray = this.$refs.thronesTextData.textData;
+        const randomIndex = Math.floor(Math.random() * textDataArray.length);
+        this.textData = textDataArray[randomIndex];
+      } else {
+        console.error("ThronesTextData component or its textData is undefined");
+      }
+    },
+    changeColor() {
+      let newIndex;
+      do {
+        newIndex = Math.floor(Math.random() * this.colorClasses.length);
+      } while (newIndex === this.currentClassIndex);
+      this.currentClassIndex = newIndex;
+    },
+    handleClick() {
+      this.generateText();
+      this.changeColor();
+    },
+  },
+  mounted() {
+    // Initialize textData with the first element of thronesTextData
+    this.$nextTick(() => {
+      if (this.$refs.thronesTextData && this.$refs.thronesTextData.textData) {
+        this.textData = this.$refs.thronesTextData.textData[0];
+      } else {
+        console.error("ThronesTextData component or its textData is undefined");
+      }
+    });
   },
 };
 </script>
-
-<template v-cloak>
-  <div class="text-Font">
-    <div class="background">
-      <div class="container mx-auto text-thrones">
-        <div class="text text-2xl md:text-6xl sm:text-xl py-12 text-center">
-          <h1 class="py-2">
-            <span class="md:text-7xl text-3xl daysText">{{ daysPassed }}</span>
-            DAYS SINCE LAST
-          </h1>
-          <h1 class="py-2">ASOIAF BOOK</h1>
-          <h2 class="py-2 text-2xl">in the mean time...</h2>
-        </div>
-      </div>
-    </div>
-    <div class="border-t-8 border-[#010101]"></div>
-    <div class="textFont font-semibold">
-      <div class="background2 py-12">
-        <CountingDiv />
-      </div>
-      <div class="background p-10">
-        <div class="text text-3xl md:text-3xl py-10 text-left textFont">
-          You could have travelled from King's Landing to the Wall
-          <!-- 200 days is how long is ONE journey from King's landing to the Wall-->
-          <span class="text-5xl text-[#7F5AF0] font-semibold animate-pulse">{{
-            Math.ceil(daysPassed / 200)
-          }}</span>
-          times.
-          <div class="text-xs font-sans italic">
-            The distance is 3000 miles, assuming you walk 15 miles each day
-          </div>
-        </div>
-      </div>
-      <div class="background2 p-10">
-        <div class="text text-2xl md:text-3xl sm:text-xl text-right py-10">
-          You could read the current ASOIAF books
-          <!-- 47 days is how long you need to read a volume -->
-          <span class="text-5xl text-[#7F5AF0] font-semibold">{{
-            Math.ceil(daysPassed / 47)
-          }}</span>
-          times.
-          <div class="text-xs font-sans italic">
-            You read 2h per day at 250WPM
-          </div>
-        </div>
-      </div>
-      <div class="background p-10">
-        <div class="text text-2xl md:text-3xl py-10 text-left">
-          You could have watched the entire series GOT
-          <!-- 14 days or 70 / 5 is how long it takes to watch the series.. 5h per day 70 is how long is the series -->
-          <span class="text-5xl text-[#7F5AF0] font-semibold">{{
-            Math.ceil(daysPassed / (70 / 5))
-          }}</span>
-          times.
-          <div class="text-xs font-sans italic">
-            Assuming you can watch it more than once
-          </div>
-        </div>
-      </div>
-      <div class="background2 p-10">
-        <div class="text text-4xl md:text-3xl py-10 text-right">
-          You could have watched LOTR extended trilogy
-          <span class="text-5xl text-[#7F5AF0] font-semibold">{{
-            Math.ceil(daysPassed / 11.5)
-          }}</span>
-          times.
-        </div>
-      </div>
-      <div class="background p-10">
-        <div class="text text-4xl md:text-3xl py-10 text-left">
-          Metis travel around the jupiter
-          <span class="text-5xl text-[#7F5AF0] font-semibold">{{
-            Math.ceil(daysPassed / 0.3)
-          }}</span>
-          times.
-        </div>
-      </div>
-      <div class="background2 p-10">
-        <div class="text text-4xl md:text-3xl py-10 text-right">
-          Earth was hitted by
-          <span class="text-5xl text-[#7F5AF0] font-semibold">{{
-            Math.ceil(daysPassed * 30)
-          }}</span>
-          Meteors.
-        </div>
-      </div>
-      <div class="background p-10">
-        <div class="text text-4xl md:text-3xl py-10 text-left">
-          Queen Elizabeth II became the longest-reigning British monarch
-        </div>
-      </div>
-      <div class="background2 p-10">
-        <div class="text text-4xl md:text-3xl py-10 text-right">
-          The first iPad was released
-        </div>
-      </div>
-      <div class="background p-10">
-        <div class="text text-4xl md:text-3xl py-10 text-left">
-          First photo of the black hole
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
 
 <style scoped>
 @font-face {
   font-family: "Thrones";
   src: url("@/assets/Fonts/GameOfThrones.ttf") format("truetype");
+}
+
+@font-face {
+  font-family: "Inter";
+  src: url("@/assets/Fonts/Inter-Regular.ttf") format("truetype");
 }
 
 .background {
@@ -150,17 +160,160 @@ export default {
 }
 
 .text-Font {
-  font-family: "Montserrat";
-  text-transform: uppercase;
+  font-family: "Inter";
 }
 .daysText {
-  font-family: "Montserrat";
+  font-family: "Inter";
   color: #fffffe;
   font-size: 1.4em;
-  font-weight: 100;
 }
 .text-thrones {
   font-family: Thrones;
   color: #fffffe;
+}
+
+.targaryen {
+  position: relative;
+  background-color: #262222;
+  color: #fffffe;
+  border: 10px solid transparent;
+  border-radius: 15px;
+  border-image: linear-gradient(to bottom right, #ff8a00, #da1b60, #ff8a00) 1;
+  box-shadow: 0 0 10px 5px rgba(255, 0, 0, 0.7),
+    0 0 20px 10px rgba(255, 69, 0, 0.5), 0 0 30px 15px rgba(255, 140, 0, 0.3),
+    inset 0 0 10px 5px rgba(255, 0, 0, 0.7),
+    inset 0 0 20px 10px rgba(255, 69, 0, 0.5);
+  animation: fire-border 5s infinite alternate;
+}
+
+.stark {
+  position: relative;
+  background-color: #c9e9ee;
+  color: #000;
+  border: 10px solid transparent;
+  border-image: linear-gradient(to bottom right, #2b3b4a, #353331, #2b3b4a) 1;
+  box-shadow: 0 0 10px 15px rgba(43, 59, 74, 0.7),
+    0 0 20px 10px rgba(53, 51, 49, 0.5), 0 0 30px 15px rgba(0, 0, 0, 0.3),
+    inset 0 0 10px 5px rgba(43, 59, 74, 0.7),
+    inset 0 0 20px 10px rgba(53, 51, 49, 0.5);
+  animation: ice-border 5s infinite alternate;
+}
+
+.lannister {
+  position: relative;
+  background-color: #931613;
+  color: #fff;
+  border: 10px solid transparent;
+  border-image: linear-gradient(to bottom right, #f3b72e, #931613, #f3b72e) 1;
+  box-shadow: 0 0 10px 15px rgba(147, 22, 19, 0.7),
+    0 0 20px 10px rgba(147, 22, 19, 0.5), 0 0 30px 15px rgba(0, 0, 0, 0.3),
+    inset 0 0 10px 5px rgba(243, 183, 46, 0.7),
+    inset 0 0 20px 10px rgba(147, 22, 19, 0.5);
+  animation: lion-border 5s infinite alternate;
+}
+
+.baratheon {
+  position: relative;
+  background-color: #f6ef4c;
+  color: #000;
+  border: 10px solid transparent;
+  border-image: linear-gradient(to bottom right, #382929, #513f3f, #382929) 1;
+  box-shadow: 0 0 20px 15px rgba(81, 63, 63, 0.7),
+    0 0 20px 10px rgba(0, 0, 0, 0.5), 0 0 30px 25px rgba(0, 0, 0, 0.3),
+    inset 0 0 10px 5px rgba(0, 0, 0, 0.7),
+    inset 0 0 20px 10px rgba(210, 70, 80, 0.5);
+  animation: stag-border 5s infinite alternate;
+}
+
+.targaryen::before,
+.stark::before,
+.lannister::before,
+.baratheon::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%; 
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: right;
+  opacity: 0.33;
+}
+
+.targaryen::before {
+  background-image: url("/Images/Thrones/targaryen.png");
+}
+
+.stark::before {
+  background-image: url("/Images/Thrones/stark.png");
+}
+
+.lannister::before {
+  background-image: url("/Images/Thrones/lannister.png");
+}
+
+.baratheon::before {
+  background-image: url("/Images/Thrones/baratheon.png");
+}
+
+@keyframes fire-border {
+  0% {
+    border-image: linear-gradient(to bottom right, #ff8a00, #da1b60, #ff8a00) 1;
+  }
+  25% {
+    border-image: linear-gradient(to top left, #ff4500, #ff0000, #ff4500) 1;
+  }
+  50% {
+    border-image: linear-gradient(to top right, #ff8a00, #da1b60, #ff8a00) 1;
+  }
+  100% {
+    border-image: linear-gradient(to bottom left, #ff4500, #ff0000, #ff4500) 1;
+  }
+}
+
+@keyframes ice-border {
+  0% {
+    border-image: linear-gradient(to bottom right, #2b3b4a, #353331, #2b3b4a) 1;
+  }
+  25% {
+    border-image: linear-gradient(to top left, #2b3b4a, #353331, #2b3b4a) 1;
+  }
+  50% {
+    border-image: linear-gradient(to top right, #2b3b4a, #353331, #2b3b4a) 1;
+  }
+  100% {
+    border-image: linear-gradient(to bottom left, #2b3b4a, #353331, #2b3b4a) 1;
+  }
+}
+
+@keyframes lion-border {
+  0% {
+    border-image: linear-gradient(to bottom right, #f3b72e, #931613, #f3b72e) 1;
+  }
+  25% {
+    border-image: linear-gradient(to top left, #f3b72e, #631025, #f3b72e) 1;
+  }
+  50% {
+    border-image: linear-gradient(to top right, #f3b72e, #631025, #f3b72e) 1;
+  }
+  100% {
+    border-image: linear-gradient(to bottom left, #f3b72e, #931613, #f3b72e) 1;
+  }
+}
+
+@keyframes stag-border {
+  0% {
+    border-image: linear-gradient(to bottom right, #62556d, #d24646, #382929) 1;
+  }
+  25% {
+    border-image: linear-gradient(to top left, #62556d, #d24646, #382929) 1;
+  }
+  50% {
+    border-image: linear-gradient(to top right, #62556d, #d24646, #382929) 1;
+  }
+  100% {
+    border-image: linear-gradient(to bottom left, #62556d, #d24646, #382929) 1;
+  }
 }
 </style>
