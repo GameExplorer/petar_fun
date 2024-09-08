2acae862249dd1cdc9317ba22842ff5aa1d807d8
GameExplorer <petar.petrovic2001@gmail.com>
2024-09-08 20:22:58 +0200

    Update 1.29dd
    - Added analytics

--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import '@/style.css'
 import App from '@/App.vue'
 import router from '@/plugins/router.js'
 
+import { inject } from '@vercel/analytics';
+
+inject();
+
 createApp(App).use(router).mount('#app')
